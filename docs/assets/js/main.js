a46d639140575a85385c46aa4ee2d4d309fd6635
Dax Pandhi <dax@quadspinner.com>
2020-12-01 16:35:05 +0530

    Code reorg

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,39 +1,30 @@
-// Auto update layout
 if (window.layoutHelpers) {
     window.layoutHelpers.setAutoUpdate(true);
 }
-
 function changeStyle() {
     if (Cookies.get('style') == "dark") {
-        document.getElementById('cssx').href = "/assets/custom/dark.css";
+        $('head').append('<link rel="stylesheet" href="/assets/custom/dark.css" id="cssx" type="text/css" />');
         $("#logo").prop("src", "/images/Gaea_Logo_LightAlt.png");
         $("#cb1").prop("checked", true);
     } else {
-        document.getElementById('cssx').href = "/";
+        $("#cssx").remove();
         $("#logo").prop("src", "/images/Gaea_Logo_Dark.png");
         $("#cb1").prop("checked", false);
     }
 }
-
 function setStyle() {
     if (document.getElementById("cb1").checked) {
         Cookies.set('style', 'dark');
     } else {
         Cookies.set('style', 'light');
     }
-
     changeStyle();
 }
-
 $(document).ready(function () {
     $('#cb1').on('click', function () { setStyle(); });
-
     $("#small-nav-dropdown").change(function () {
         if ($(this).val() != '') { window.location.href = $(this).val(); }
     });
-
     $("video").parent().addClass("video-holder");
 });
 changeStyle();
-
-
